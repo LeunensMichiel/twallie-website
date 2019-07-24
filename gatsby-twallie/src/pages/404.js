9694888193e77371dc7d0972c7b37774431316d2
Michiel Leunens <michiel.leunens@gmail.com>
2019-07-24 18:08:45 +0200

    netlify changes

--- gatsby-twallie/src/pages/404.js
+++ gatsby-twallie/src/pages/404.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { device } from '../components/device'
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0 2em;
+  margin-top: 10%;
+  text-align: center;
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  @media ${device.tablet} {
+    margin-top: 40px;
+    h1 {
+      font-size: 72px;
+    }
+    p {
+      font-size: 18px;
+    }
+  }
+`
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: NOT FOUND</h1>
-    <p>Oei? Precies een fout nummer aangevraagd...</p>
+    <Container>
+      <h1>404 // NOT FOUND</h1>
+      <p>Oei? Precies een fout nummer aangevraagd...</p>
+    </Container>
   </Layout>
 )
 
